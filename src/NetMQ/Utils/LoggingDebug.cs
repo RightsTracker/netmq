ec8d97964e490becffeb97a4f0169d7c24df9254
Jay <garkpit@users.noreply.github.com>
2019-05-17 14:18:40 +0100

    Debug.Assert override now NLogging to "NetMQ"

--- src/NetMQ/Utils/LoggingDebug.cs
+++ src/NetMQ/Utils/LoggingDebug.cs
@@ -2,18 +2,19 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text;
+using NLog;
 
 namespace NetMQ.Utils
 {
     public class LoggingDebug
     {
+        public static Logger log = LogManager.GetLogger("NetMQ");
+
         public static void Assert(bool condition)
         {
             if (condition) return;
 
-            //TODO: log here with something else - maybe NLOG?
-            Debug.WriteLine("LogginDebug.Assert - condition was not met");
-            return;
+            log.Warn($"Assert false! At {Environment.StackTrace}");
         }
     }
 }
