0e1952a207247ee9bacb622d99e68d881092e319
Jay <garkpit@users.noreply.github.com>
2019-05-22 07:37:14 +0100

    XML comment warning fixed

--- src/NetMQ/Utils/LoggingDebug.cs
+++ src/NetMQ/Utils/LoggingDebug.cs
@@ -1,15 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Text;
 using NLog;
 
 namespace NetMQ.Utils
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class LoggingDebug
     {
+        /// <summary>
+        /// 
+        /// </summary>
         public static Logger log = LogManager.GetLogger("NetMQ");
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="condition"></param>
         public static void Assert(bool condition)
         {
             if (condition) return;
