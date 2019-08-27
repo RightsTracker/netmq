057867bc9bb1a3ad91638d03c6fc9daf0a016513
Jay <garkpit@users.noreply.github.com>
2019-08-27 17:01:27 +0100

    + CopyDllsToAvailabilitiesEngine.bat

---
+++ src/CopyDllsToAvailabilitiesEngine.bat
@@ -0,0 +1,3 @@
+copy NetMQ\bin\Debug\netstandard2.0\NetMQ.dll ..\..\AvailabilitiesEngine\External\Anthill\
+copy NetMQ\bin\Debug\netstandard2.0\NetMQ.pdb ..\..\AvailabilitiesEngine\External\Anthill\
+copy NetMQ\bin\Debug\netstandard2.0\NetMQ.deps.json ..\..\AvailabilitiesEngine\External\Anthill\
